2e881cfd0328f66f8409442353b6f0b59fa6b7fb
Filip Stenbacka <filip.stenbacka@trimble.com>
2021-06-28 23:41:22 +0300

    v.1.0.0

--- src/__tests__/dlux.test.ts
+++ src/__tests__/dlux.test.ts
@@ -43,58 +43,93 @@ test("STATIC RGB color", () => {
 test("PATTERN HV color", () => {
   const o: IScenePattern = {
     type: "PATTERN",
-    pattern: [[[0x01, 0x02], 5], [[0x02, 0x03], 2], [[0x10, 0x09], 3]],
+    pattern: [
+      [[0x01, 0x02], 5],
+      [[0x02, 0x03], 2],
+      [[0x10, 0x09], 3],
+    ],
   };
 
   expect(encode(o)).toEqual({ scene: 2, data: Buffer.from([0x01, 0x02, 5, 0x02, 0x03, 2, 0x10, 0x09, 3]) });
 
-  o.pattern = [[[-1, 0], -1], [[0x02, 0x03], 0], [[255, 256], 257]];
+  o.pattern = [
+    [[-1, 0], -1],
+    [[0x02, 0x03], 0],
+    [[255, 256], 257],
+  ];
   expect(encode(o)).toEqual({ scene: 2, data: Buffer.from([0x0ff, 0x0ff, 0xff]) });
 });
 
 test("PATTERN RGB color", () => {
   const o: IScenePattern = {
     type: "PATTERN",
-    pattern: [[[0x01, 0x02, 0x03], 5], [[0x02, 0x03, 0x04], 2], [[0x10, 0x09, 0x08], 3]],
+    pattern: [
+      [[0x01, 0x02, 0x03], 5],
+      [[0x02, 0x03, 0x04], 2],
+      [[0x10, 0x09, 0x08], 3],
+    ],
   };
 
   expect(encode(o)).toEqual({ scene: 22, data: Buffer.from([0x01, 0x02, 0x03, 5, 0x02, 0x03, 0x04, 2, 0x10, 0x09, 0x08, 3]) });
 
-  o.pattern = [[[-1, 0, -3], -1], [[0x02, 0x03, 0x04], 0], [[255, 256, 257], 257]];
+  o.pattern = [
+    [[-1, 0, -3], -1],
+    [[0x02, 0x03, 0x04], 0],
+    [[255, 256, 257], 257],
+  ];
   expect(encode(o)).toEqual({ scene: 22, data: Buffer.from([0x0ff, 0x0ff, 0xff, 0xff]) });
 });
 
 test("PATTERN hue color", () => {
   const o: IScenePattern = {
     type: "PATTERN",
-    pattern: [[0x01, 5], [0x02, 2], [0x03, 3]],
+    pattern: [
+      [0x01, 5],
+      [0x02, 2],
+      [0x03, 3],
+    ],
   };
 
   expect(encode(o)).toEqual({ scene: 42, data: Buffer.from([0x01, 5, 0x02, 2, 0x03, 3]) });
 
-  o.pattern = [[-1, -1], [0x10, 0], [260, 260]];
+  o.pattern = [
+    [-1, -1],
+    [0x10, 0],
+    [260, 260],
+  ];
   expect(encode(o)).toEqual({ scene: 42, data: Buffer.from([0x0ff, 0xff]) });
 });
 
 test("PATTERN throw", () => {
   const o: IScenePattern = {
     type: "PATTERN",
-    pattern: [[0x01, 5], [[0x01, 0x02], 2]],
+    pattern: [
+      [0x01, 5],
+      [[0x01, 0x02], 2],
+    ],
   };
 
   // Color type mismatch
   expect(() => encode(o)).toThrow();
 
-
   // Color type mismatch
-  o.pattern = [[0x01, 5], [[0x01, 0x02, 0x03], 2]];
+  o.pattern = [
+    [0x01, 5],
+    [[0x01, 0x02, 0x03], 2],
+  ];
   expect(() => encode(o)).toThrow();
 
   // Color type mismatch
-  o.pattern = [[[0x01, 0x02, 0x03], 2], [0x01, 5]];
+  o.pattern = [
+    [[0x01, 0x02, 0x03], 2],
+    [0x01, 5],
+  ];
   expect(() => encode(o)).toThrow();
 
   // Color type mismatch
-  o.pattern = [[[0x01, 0x02], 2], [0x01, 5]];
+  o.pattern = [
+    [[0x01, 0x02], 2],
+    [0x01, 5],
+  ];
   expect(() => encode(o)).toThrow();
 });
