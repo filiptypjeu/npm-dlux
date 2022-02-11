292ade8e022e369b5092a17c2772cd46d6d3f564
Filip Stenbacka <filip.stenbacka@gmail.com>
2022-02-11 20:03:30 +0200

    Added missing export.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export * from "./led/types";
 export * from "./mqtt/DluxMqttDevice";
 export * from "./mqtt/DluxLedDevice";
 export * from "./mqtt/DluxLedMultiDevice";
+export * from "./mqtt/DluxLampDevice";
