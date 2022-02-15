4dc2a752994f0d123cbf45228d1782e1f250d727
Filip Stenbacka <filip.stenbacka@gmail.com>
2022-02-15 02:17:31 +0200

    Added tests using the mocked client class.

---
+++ src/__tests__/dlux-mqtt-device-mocked.test.ts
@@ -0,0 +1,85 @@
+import { DluxMqttDevice } from "../index";
+import { MqttClientMock } from "./MqttClientMock.test";
+
+const client = new MqttClientMock();
+
+const d = new DluxMqttDevice({
+  name: "device",
+  topic: "dlux/l1",
+});
+d.initialize(client);
+
+test("mocked client inital publishes", () => {
+  expect(client.publishes).toHaveLength(1);
+  expect(client.publishes).toEqual([{ topic: "dlux/l1", payload: "v" }]);
+});
+
+test("mocked client inital subscriptions", () => {
+  expect(client.subscriptions).toHaveLength(5);
+  expect(client.subscriptions).toEqual([
+    "dlux/l1/status",
+    "dlux/l1/version",
+    "dlux/l1/log",
+    "dlux/l1/inputs",
+    "dlux/l1/outputs",
+  ]);
+});
+
+test("mocked client inital listeners", () => {
+  expect(client.listeners).toHaveLength(1);
+});
+
+test("dlux mqtt device mocked status topic", () => {
+  expect(d.online).toEqual(false);
+  client.mock("dlux/l1/status", Buffer.from("online"));
+  expect(d.online).toEqual(true);
+  client.mock("dlux/l1/status", Buffer.from("disconnected"));
+  expect(d.online).toEqual(false);
+});
+
+test("dlux mqtt device mocked version topic", () => {
+  expect(d.version).toEqual("");
+  client.mock("dlux/l1/version", Buffer.from("v.1.2"));
+  expect(d.version).toEqual("v.1.2");
+  client.mock("dlux/l1/version", Buffer.from("v.1.3"));
+  expect(d.version).toEqual("v.1.3");
+});
+
+test("dlux mqtt device mocked log topic", () => {
+  client.mock("dlux/l1/log", Buffer.from("Version = v.1.4"));
+  expect(d.version).toEqual("v.1.4");
+  client.mock("dlux/l1/log", Buffer.from("Version = v.1.5"));
+  expect(d.version).toEqual("v.1.5");
+});
+
+test("dlux mqtt device mocked inputs topic", () => {
+  expect(d.inputs[0]).toEqual(undefined);
+  expect(d.inputs[1]).toEqual(undefined);
+  expect(d.inputs[2]).toEqual(undefined);
+  expect(d.inputs[3]).toEqual(undefined);
+  expect(d.inputs[4]).toEqual(undefined);
+  client.mock("dlux/l1/inputs", Buffer.from("011:1:-:0"));
+  expect(d.inputs[0]).toEqual(11);
+  expect(d.inputs[1]).toEqual(true);
+  expect(d.inputs[2]).toEqual(undefined);
+  expect(d.inputs[3]).toEqual(false);
+  expect(d.inputs[4]).toEqual(undefined);
+});
+
+test("dlux mqtt device mocked outputs topic", () => {
+  expect(d.outputs[0]).toEqual(undefined);
+  expect(d.outputs[1]).toEqual(undefined);
+  expect(d.outputs[2]).toEqual(undefined);
+  expect(d.outputs[3]).toEqual(undefined);
+  client.mock("dlux/l1/outputs", Buffer.from("-10"));
+  expect(d.outputs[0]).toEqual(undefined);
+  expect(d.outputs[1]).toEqual(true);
+  expect(d.outputs[2]).toEqual(false);
+  expect(d.outputs[3]).toEqual(undefined);
+});
+
+test("mocked client state after tests", () => {
+  expect(client.publishes).toHaveLength(1);
+  expect(client.subscriptions).toHaveLength(5);
+  expect(client.listeners).toHaveLength(1);
+});
